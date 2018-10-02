4fcc3326baa85acd812d27421f3e397b917c5524
Anish Malhotra <technophile.m7@gmail.com>
2018-10-02 10:42:48 +0530

    Add files via upload

---
+++ Array/fibonacci.cpp
@@ -0,0 +1,41 @@
+
+
+#include <iostream>
+#include<cstdio>
+using namespace std;
+
+const long long MAX = 93;
+ 
+
+long long f[MAX] = {0};
+ 
+
+long long fib(long long n)
+{
+
+    if (n == 0)
+        return 0;
+    if (n == 1 || n == 2)
+        return (f[n] = 1);
+ 
+
+    if (f[n])
+        return f[n];
+ 
+    long long k = (n%2!=0)? (n+1)/2 : n/2;
+ 
+    f[n] = (n%2!=0)? (fib(k)*fib(k) + fib(k-1)*fib(k-1))
+           : (2*fib(k-1) + fib(k))*fib(k);
+    return f[n];
+}
+
+
+int main()
+{
+	//Main Function
+	for(long long i=1;i<93;i++)
+	{
+		cout << i << " th fibonacci number is " << fib(i) << "\n";		
+	}
+	return 0;
+}
